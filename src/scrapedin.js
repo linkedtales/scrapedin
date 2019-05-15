75c27eb2761392cd850dd5e42546bc3edbe2389c
Wagner Leonardi <leonardiwagner@gmail.com>
2019-05-15 11:29:34 -0300

    linkedin schema updates

--- src/scrapedin.js
+++ src/scrapedin.js
@@ -3,7 +3,7 @@ const login = require('./login')
 const profile = require('./profile')
 const logger = require('./logger')
 
-module.exports = ({ email, password, isHeadless, hasToLog, proxyAddress } = { isHeadless: true, hasToLog: false }) => new Promise(async (resolve, reject) => {
+module.exports = async({ email, password, isHeadless, hasToLog, puppeteerArgs } = { isHeadless: true, hasToLog: false }) => {
   if (!hasToLog) {
     logger.stopLogging()
   }
@@ -11,21 +11,20 @@ module.exports = ({ email, password, isHeadless, hasToLog, proxyAddress } = { is
 
   if (!email || !password) {
     logger.warn('scrapedin', 'required parameters email and password was not provided')
-    return reject(new Error('scrapedin: email and password are required to access linkedin profiles'))
+    throw new Error('scrapedin: email and password are required to access linkedin profiles')
   }
 
   logger.info('scrapedin', 'required parameters email and password was provided')
 
-  const args = proxyAddress && [`--proxy-server=${proxyAddress}`]
-  args && logger.info('scrapedin', `using proxy address: ${proxyAddress}`)
-  const browser = await puppeteer.launch({ headless: isHeadless, args })
+  const args = Object.assign({ headless: isHeadless }, puppeteerArgs)
+  const browser = await puppeteer.launch(args)
 
   try {
     await login(browser, email, password, logger)
   } catch (e) {
     await browser.close()
-    return reject(e)
+    throw e
   }
 
-  return resolve(Promise.resolve((url, waitMs) => profile(browser, url, waitMs)))
-})
+  return (url, waitMs) => profile(browser, url, waitMs)
+}
