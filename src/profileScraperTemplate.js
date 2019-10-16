eb8e0fe7f85b4c0156c216b6a6fbfb46cff4650b
Wagner Leonardi <leonardiwagner@gmail.com>
2019-10-16 13:08:41 -0300

    see more improvements

--- src/profileScraperTemplate.js
+++ src/profileScraperTemplate.js
@@ -62,15 +62,15 @@ module.exports = {
       title: 'h3',
       degree: 'span[class=pv-entity__comma-item]',
 	  fieldofstudy: 'p.pv-entity__fos span:nth-child(2)',
-      date1: 'p.pv-entity__dates time:nth-child(1)',
-      date2: 'p.pv-entity__dates time:nth-child(2)'
+      date1: '.pv-entity__dates time:nth-child(1)',
+      date2: '.pv-entity__dates time:nth-child(2)'
     }
   },
   skills: {
     selector: '.pv-skill-category-entity__skill-wrapper',
     fields: {
-      title: 'span.pv-skill-category-entity__name-text',
-      count: 'span.pv-skill-category-entity__endorsement-count'
+      title: '.pv-skill-category-entity__name-text',
+      count: '.pv-skill-category-entity__endorsement-count'
     }
   },
   recommendationsCount: {
@@ -84,7 +84,7 @@ module.exports = {
     selector: 'artdeco-tabpanel[aria-hidden=false] li.pv-recommendation-entity',
     fields: {
       user: {
-        selector: 'a.pv-recommendation-entity__member',
+        selector: '.pv-recommendation-entity__member',
         attribute: 'href'
       },
       text: 'blockquote.pv-recommendation-entity__text'
@@ -94,7 +94,7 @@ module.exports = {
     selector: 'artdeco-tabpanel[aria-hidden=true] li.pv-recommendation-entity',
     fields: {
       user: {
-        selector: 'a.pv-recommendation-entity__member',
+        selector: '.pv-recommendation-entity__member',
         attribute: 'href'
       },
       text: 'blockquote.pv-recommendation-entity__text'
@@ -103,8 +103,8 @@ module.exports = {
   accomplishments: {
     selector: 'section.pv-accomplishments-section div.ember-view',
     fields: {
-      count: 'h3.pv-accomplishments-block__count span:nth-child(2)',
-      title: '.accomplishments-block__content h3.pv-accomplishments-block__title',
+      count: '.pv-accomplishments-block__count span:nth-child(2)',
+      title: '.accomplishments-block__content .pv-accomplishments-block__title',
       items: {
         selector: 'li',
         isMultipleFields: true
@@ -126,9 +126,9 @@ module.exports = {
     fields: {
       title: 'h3',
       experience: 'span[class=pv-entity__secondary-title]',
-      location: 'pv-entity__location span:nth-child(2)',
+      location: '.pv-entity__location span:nth-child(2)',
       description: '.pv-volunteer-causes',
-      date1: 'h4.pv-entity__date-range span:nth-child(2)',
+      date1: '.pv-entity__date-range span:nth-child(2)',
       date2: '.pv-entity__bullet-item'
     }
   }
--- src/seeMoreButtons.js
+++ src/seeMoreButtons.js
@@ -1,35 +1,39 @@
 const logger = require('./logger')
 const seeMoreButtons = [
   {
-    id: 'SEE_MORE_EXPERIENCE_DESCRIPTION',
-    selector: '.lt-line-clamp__more'
+    id: 'SHOW_MORE_SECTIONS',
+    //button to avoid followers click which are <a> and will open a modal
+    selector: 'button.pv-profile-section__card-action-bar',
+    count: 1
   },{
     id: 'SHOW_MORE_EXPERIENCES',
     //buton to avoid activities click which are <a> and will redirect to other page
-    selector: 'button.pv-profile-section__see-more-inline'
+    selector: 'button.pv-profile-section__see-more-inline',
+    count: 3
   },{
-    id: 'SHOW_MORE_SECTIONS',
-    //button to avoid followers click which are <a> and will open a modal
-    selector: 'button.pv-profile-section__card-action-bar'
+    id: 'SEE_MORE_EXPERIENCE_DESCRIPTION',
+    selector: '.lt-line-clamp__more',
+    count: 1
   }
 ]
 
 const clickAll = async(page) => {
   for(let i = 0; i < seeMoreButtons.length; i ++){
     const button = seeMoreButtons[i]
-    if (button.selector) {
-      const elem = await page.$(button.selector)
-      if (elem) {
-        await elem.click()
-          .catch((e) => logger.warn('seeMoreButtons', `couldn't click on ${button.selector}, it's probably invisible`))
-      }
-    }
+    const elems = await page.$$(button.selector)
 
-    if (button.selectors) {
-      const elems = await page.$$(button.selectors)
-      for (let j = 0; j < elems.length; j++) {
-        await elems[j].click()
-          .catch((e) => logger.warn('seeMoreButtons', `couldn't click on ${button.selectors}, it's probably invisible`))
+    //may click multiple times (since linkedin loads 5 per click)
+    for(let j = 0; j < button.count; j++){
+      elems.map(async(elem) => {
+        if (elem) {
+          await elem.click()
+            .catch((e) => logger.warn('seeMoreButtons', `couldn't click on ${button.selector}, it's probably invisible`))
+        }
+      })
+
+      if(button.count > 1){
+        //wait for more items load
+        await new Promise((resolve) => { setTimeout(() => { resolve() }, 100)})
       }
     }
   }
