d75515ab669b9ce9d075b4d37e5891a84b11836a
Mercy Muchai <muchaimercyw@gmail.com>
2020-03-13 02:42:18 +0300

    check if cookies are being used before logging out

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -50,7 +50,9 @@ module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, hasToGet
   const volunteerExperience = await scrapSection(page, template.volunteerExperience)
   const peopleAlsoViewed = await scrapSection(page, template.peopleAlsoViewed)
 
-  await page.goto("https://www.linkedin.com/m/logout/")
+  if(!cookies) {
+    await page.goto("https://www.linkedin.com/m/logout/")
+  }
   await page.close()
   logger.info('profile', `finished scraping url: ${url}`)
 
