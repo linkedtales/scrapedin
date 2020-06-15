d5d9c02484e84d8dc7c9de60715f797c82fc5830
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2020-06-15 11:18:25 +0100

    fix: fix NODE element error caused by .click

--- src/profile/scrapAccomplishmentPanel.js
+++ src/profile/scrapAccomplishmentPanel.js
@@ -2,21 +2,19 @@ const scrapSection = require('../scrapSection');
 const template = require('./profileScraperTemplate');
 
 const scrapAccomplishmentPanel = async (page, section) => {
-  const openingButton = await page.$(
-    `.pv-accomplishments-block.${section} button`,
-  );
-  
-  if(openingButton) {
-    await openingButton.click();
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 500);
-    });
+  const queryString = `.pv-accomplishments-block.${section} button`
+
+  const openingButton = await page.$(queryString);
+
+  if (openingButton) {
+    await page.evaluate((q) => {
+      document.querySelector(q).click();
+    }, queryString);
 
     return scrapSection(page, template[section]);
+  } else {
+    return new Promise();
   }
-  
 };
 
 module.exports = scrapAccomplishmentPanel;
