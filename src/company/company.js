b09de1378f4ceed30e900e00843259e1cc6cdffb
grapevineai <dev@grapevine.ai>
2020-06-25 14:12:29 +0200

    Company scrape actions

--- src/company/company.js
+++ src/company/company.js
@@ -1,10 +1,35 @@
 const openPage = require('../openPage')
 const scrapSection = require('../scrapSection')
+const template = require('./companyScraperTemplate')
 
 const logger = require('../logger')(__filename)
 
-module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500) => {
-  logger.info(`starting scraping url: ${url}`)
+module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, puppeteerAuthenticate = undefined) => {
+  logger.info(`starting scraping url: ${url}`);
 
-  //TODO: implement company scraper
+  let company = {};
+
+  let page;
+  if(url.includes('legacySchoolId=')){
+      page = await openPage({ browser, cookies, url, puppeteerAuthenticate });
+
+      const aboutSelector = 'a[data-control-name=page_member_main_nav_about_tab]';
+
+      company.url = page.url();
+      
+      await page.$eval(aboutSelector, async about => await about.click());
+      await page.waitForNavigation();
+  } else{
+      company.url = url;
+      url = url + '/about';
+      page = await openPage({ browser, cookies, url, puppeteerAuthenticate });
+  }
+  company.about = (await scrapSection(page, template.about))[0];
+  company.profile = (await scrapSection(page, template.profile))[0];
+
+  await page.close();
+  logger.info(`finished scraping url: ${url}`);
+  
+  return company
+    
 }
